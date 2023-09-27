60a267d680e50753d3b9a977871f601f7561d58b
Shanky1001 <sr1071995@gmail.com>
2023-09-27 14:15:57 +0530

    running app offline

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,27 @@
-console.log("Service Worker File is running");
+console.log("Service Worker installed");
+let cacheVersion = "appV1";
+// Setting the cache
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheVersion).then((cache) => {
+            cache.addAll([
+                "/static/js/bundle.js","/"
+            ])
+        }).catch((err) => {
+            console.log("failed to cache")
+        })
+    )
+})
+
+// Consuming the cache
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => {
+            if (res) {
+                return res
+            }
+        }).catch(() => {
+            console.log("failed to fetch from cache")
+        })
+    )
+})
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar.tsx';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <NavBar />
     </div>
   );
 }
