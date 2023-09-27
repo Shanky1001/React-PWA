ace26cb57036bdda6f33ff065621dac8dca63fee
Shanky1001 <sr1071995@gmail.com>
2023-09-27 15:02:35 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar.tsx';
+import Home from './pages/Home.tsx';
+import Users from './pages/Users.tsx';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
